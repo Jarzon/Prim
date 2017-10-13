9e72206d9f1156e54cfd3d55d5b4427bab4ef642
Jarzon <j@masterj.net>
2017-10-12 22:48:11 -0400

    Add methods to the Model to help build basic update and insert queries

--- src/Model.php
+++ src/Model.php
@@ -3,10 +3,47 @@ namespace Prim;
 
 class Model
 {
-
     function __construct(\PDO $db)
     {
         $this->db = $db;
     }
 
+    public function update(string $table, array $data, string $where = '', array $whereValues = [])
+    {
+        $columns = [];
+        $values = [];
+
+        foreach ($data as $i => $d) {
+            $columns[] = "$i=?";
+            $values[] = $d;
+        }
+
+        if($where !== '') {
+            $where = "WHERE $where";
+            array_merge($values, $whereValues);
+        }
+
+        $query = $this->db->prepare("UPDATE $table SET ".implode(',', $columns)." $where");
+
+        $query->execute($values);
+    }
+
+    public function insert(string $table, array $data)
+    {
+        $columns = [];
+        $values = [];
+
+        foreach ($data as $i => $d) {
+            $columns[] = $i;
+            $values[] = $d;
+        }
+
+        $columns = implode(',', $columns);
+        $placeholders = implode(',', str_split(str_repeat('?', sizeof($columns))));
+        $values = implode(',', $values);
+
+        $query = $this->db->prepare("INSERT INTO $table ($columns) VALUES($placeholders)");
+
+        $query->execute($values);
+    }
 }
