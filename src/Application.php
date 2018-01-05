969aae26d63376aa9730bc0dd08d1c607bee28d0
Jarzon <j@masterj.net>
2018-01-05 18:40:00 -0500

    Handle error/exeptions Send a mail on error Show custom page on internal server error

--- src/Application.php
+++ src/Application.php
@@ -10,15 +10,18 @@ class Application
     public $container;
     public $router;
     public $projectNamespace;
+    protected $errorController;
 
-    /**
-     * Routing
-     */
     public function __construct(Container $container, Controller $error)
     {
+        $this->errorController = $error;
         $this->container = $container;
 
         $this->projectNamespace = $error->projectNamespace;
+
+        register_shutdown_function( [$this, 'checkFatal'] );
+        set_error_handler( [$this, 'logError'] );
+        set_exception_handler( [$this, 'logException'] );
 
         if(ENV == 'prod') {
             define('URL_RELATIVE_BASE', $_SERVER['REQUEST_URI']);
@@ -72,5 +75,50 @@ class Application
                 $controller->$method(...$vars);
                 break;
         }
+    }
+
+    /**
+     * Uncaught exception handler.
+     */
+    public function logException($e)
+    {
+        if (DEBUG == true ) {
+            echo $this->errorController->debug($e);
+        }
+        else {
+            $headers[] = 'MIME-Version: 1.0';
+            $headers[] = 'To: ' . ERROR_MAIL;
+            $headers[] = 'From: ' . ERROR_MAIL_FROM;
+
+            $message[] = 'Type: ' . get_class($e);
+            $message[] = "Message: {$e->getMessage()}";
+            $message[] = "File: {$e->getFile()}";
+            $message[] = "Line: {$e->getLine()}";
+            $message = wordwrap(implode("\r\n", $message), 70, "\r\n");
+
+            mail(ERROR_MAIL, 'PHP Error', $message, implode("\r\n", $headers));
+
+            echo $this->errorController->handleError(500);
+            exit;
+        }
+    }
+
+    /**
+     * Checks for a fatal error, work around for set_error_handler not working on fatal errors.
+     */
+    public function checkFatal()
+    {
+        $error = error_get_last();
+        if ($error['type'] == E_ERROR) {
+            $this->logError($error['type'], $error['message'], $error['file'], $error['line']);
+        }
+    }
+
+    /**
+     * Error handler, passes flow over the exception logger with new ErrorException.
+     */
+    public function logError($num, $str, $file, $line, $context = null)
+    {
+        $this->logException(new \ErrorException( $str, 0, $num, $file, $line ));
     }
 }
