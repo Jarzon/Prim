db7a12936addafa5d69b387e96edea5064fbc697
Jarzon <j@masterj.net>
2018-01-12 15:00:49 -0500

    Add PHPDoc return class hint to the Container that IDEs can use Make a base array for the parameters and overwrite it with the container config to avoid error when missing a class in the project config after an update

--- src/Container.php
+++ src/Container.php
@@ -5,14 +5,14 @@ class Container
 {
     static protected $shared = array();
 
-    public function __construct(array $parameters = [
-        'view.class' => '\\Prim\\View',
-        'router.class' => '\\Prim\\Router',
-        'pdo.class' => '\PDO',
-        'errorController.class' => 'PrimPack\Controller\Error'
-    ])
+    public function __construct(array $parameters = [])
     {
-        $this->parameters = $parameters;
+        $this->parameters = array_merge([
+            'view.class' => '\Prim\View',
+            'router.class' => '\Prim\Router',
+            'pdo.class' => '\PDO',
+            'errorController.class' => 'PrimPack\Controller\Error'
+        ], $parameters);
     }
 
     private function init(string $name, ...$args) {
@@ -28,6 +28,9 @@ class Container
         return self::$shared[$name] = $obj;
     }
 
+    /**
+     * @return Router
+     */
     public function getRouter(\FastRoute\RouteCollector $router)
     {
         $obj = 'router';
@@ -35,6 +38,9 @@ class Container
         return $this->init($obj, $router);
     }
 
+    /**
+     * @return View
+     */
     public function getView()
     {
         $obj = 'view';
@@ -42,6 +48,9 @@ class Container
         return $this->init($obj, $this);
     }
 
+    /**
+     * @return Controller
+     */
     public function getController(string $obj)
     {
         $this->parameters["$obj.class"] = $obj;
@@ -49,6 +58,9 @@ class Container
         return $this->init($obj, $this->getView(), $this);
     }
 
+    /**
+     * @return Controller
+     */
     public function getErrorController()
     {
         $obj = 'errorController';
@@ -56,6 +68,9 @@ class Container
         return $this->init($obj, $this->getView(), $this);
     }
 
+    /**
+     * @return Model
+     */
     public function getModel(string $obj)
     {
         $this->parameters["$obj.class"] = $obj;
@@ -63,6 +78,9 @@ class Container
         return $this->init($obj, $this);
     }
 
+    /**
+     * @return \PDO
+     */
     public function getPDO(string $type = '', string $host = '', string $name = '', string $charset = '', string $user = '', string $pass = '', array $options = [])
     {
         $obj = 'pdo';
