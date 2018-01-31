b46928df43cb3dc4eedd642885f3fce5585f89a3
Jarzon <j@masterj.net>
2018-01-31 00:00:59 -0500

    Add Composer Mock in tests

---
+++ tests/Mocks/Composer.php
@@ -0,0 +1,9 @@
+<?php
+namespace Tests\Mocks;
+
+use Composer\Autoload\ClassLoader;
+
+class Composer extends ClassLoader
+{
+
+}
--- tests/Mocks/Container.php
+++ tests/Mocks/Container.php
@@ -3,12 +3,8 @@ namespace Tests\Mocks;
 
 class Container extends \Prim\Container
 {
-    public function __construct(array $parameters = [
-        'view.class' => '\Tests\Mocks\View',
-        'router.class' => '\\Prim\\Router',
-        'pdo.class' => '\Tests\Mocks\PDO'
-    ])
+    public function getComposer(): object
     {
-        parent::__construct($parameters);
+        return new Composer();
     }
 }
