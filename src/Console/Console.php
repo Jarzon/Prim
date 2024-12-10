4b436f8bd0f260922deb51d16ff47f3733fef47e
Jarzon <jason@libellum.ca>
2024-12-10 16:46:31 -0500

    Add error reports in Console

--- src/Console/Console.php
+++ src/Console/Console.php
@@ -14,6 +14,8 @@ class Console
     protected Input $input;
     protected Output $output;
 
+    protected bool $errorReported = false;
+
     public function __construct(Container $container, string $root, Input $input = null, Output $output = null, array $commands = null)
     {
         $this->root = $root;
@@ -31,7 +33,51 @@ class Console
         $this->input = $input;
         $this->output = $output;
 
+        $this->setErrorHandlers();
+
         if($commands === null) include("{$this->root}app/config/commands.php");
+    }
+
+    public function setErrorHandlers(): void
+    {
+        register_shutdown_function( [$this, 'checkFatal'] );
+        set_error_handler( [$this, 'logError'] ); /** @phpstan-ignore-line */
+        set_exception_handler( [$this, 'logException'] );
+    }
+    /**
+     * Uncaught exception handler.
+     */
+    public function logException(\Throwable $e): void
+    {
+        if($this->errorReported) return;
+        $this->errorReported = true;
+
+        echo $this->container->get('errorController')->logError($e);
+
+        if(get_class($e) === 'ErrorException') {
+            throw $e;
+        } else {
+            exit;
+        }
+    }
+
+    /**
+     * Checks for a fatal error, work around for set_error_handler not working on fatal errors.
+     */
+    public function checkFatal(): void
+    {
+        $error = error_get_last();
+        if (!empty($error)) {
+            $this->logError($error['type'], $error['message'], $error['file'], $error['line']);
+        }
+    }
+
+    /**
+     * Used with set_error_handler to convert some errors to exceptions.
+     */
+    public function logError(int $num, string $str, string $file, int $line): void
+    {
+        throw new \ErrorException( $str, 0, $num, $file, $line );
     }
 
     function run(): void
